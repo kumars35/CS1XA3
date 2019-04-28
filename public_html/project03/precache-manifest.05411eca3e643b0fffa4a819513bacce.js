bbd5180fd9b8b6ad984fb5dbcaffe868b04257b9
kumars35 <kumars35@mcmaster.ca>
2019-04-28 19:09:28 +0000

    Project 03 Submission

---
+++ public_html/project03/precache-manifest.05411eca3e643b0fffa4a819513bacce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883e8384b577ff6eca2500f4e8501574",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3f67f975f7b8016e730",
+    "url": "./static/css/main.c516d070.chunk.css"
+  },
+  {
+    "revision": "87b30653a0691ad241b3",
+    "url": "./static/js/2.a766f99a.chunk.js"
+  },
+  {
+    "revision": "c3f67f975f7b8016e730",
+    "url": "./static/js/main.d13d34a6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ public_html/project03/precache-manifest.e195e42930917960f618d5acce72d931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbc3e923e9bc4fb11dc11b570d09990",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5aae7610bfa013ade880",
+    "url": "./static/css/main.c516d070.chunk.css"
+  },
+  {
+    "revision": "87b30653a0691ad241b3",
+    "url": "./static/js/2.a766f99a.chunk.js"
+  },
+  {
+    "revision": "5aae7610bfa013ade880",
+    "url": "./static/js/main.dbd3e26c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
